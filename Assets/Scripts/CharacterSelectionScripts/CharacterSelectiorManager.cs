21fb8efeb98953c8a0321892d9f15c934e0b373d
Rodbena <42215241+Rodbena@users.noreply.github.com>
2020-10-18 15:45:39 -0400

    Character Selection
    We're able to select different characters now

--- Assets/Scripts/CharacterSelectionScripts/CharacterSelectiorManager.cs
+++ Assets/Scripts/CharacterSelectionScripts/CharacterSelectiorManager.cs
@@ -6,25 +6,29 @@ public class CharacterSelectiorManager : MonoBehaviour
 {   
     public bool characterIsSelected = false;
 
-    public int[] CharId;
+    public int CharId;
     GameObject go;
    // GameObject ch1;
    // GameObject ch2;
     
     void OnTriggerEnter2D(Collider2D col)
     {
-        if(col.gameObject.CompareTag("Cursor")){
+        if(col.gameObject.CompareTag("Cursor") && CharId == 1){
             
             go.GetComponent<CursorMovement>().characterIsSelected = true;
-    
-           
-            
-            
+            Debug.Log("first char ");
+
         }
+
+        if(col.gameObject.CompareTag("Cursor") && CharId == 2){
+            
+            go.GetComponent<CursorMovement>().characterIsSelected = true;
+            Debug.Log("second char ");
+
+        }
+        
     }
-    private void OnTriggerExit2D(Collider2D col) {
-        go.GetComponent<CursorMovement>().characterIsSelected = false;
-    }
+
 
     private void Start() {
          go = GameObject.Find("CursorP1");
